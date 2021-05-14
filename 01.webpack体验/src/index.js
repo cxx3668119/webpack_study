3f6e35e50f8c96c012ee4db4dfde71ddc8d82062
cxx3668119 <862704268@qq.com>
2021-05-14 10:01:56 +0800

    day01

--- 01.webpack体验/src/index.js
+++ 01.webpack体验/src/index.js
@@ -1,4 +1,9 @@
 import { sum, mul } from "./js/math.js";
+const { dateFormat, priceFormat } = require('./js/format');
+
 
 console.log(sum(20, 30));
 console.log(mul(20, 30));
+
+console.log(dateFormat("1"));
+console.log(priceFormat("1"));
